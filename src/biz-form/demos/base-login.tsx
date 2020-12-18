cd091877c243543668e615ffa5fb066d65723aed
蔡金锋 <jeff.cai@99bill.com>
2020-12-18 20:29:17 +0800

    docs: 更新示例

--- src/biz-form/demos/base-login.tsx
+++ src/biz-form/demos/base-login.tsx
@@ -1,8 +1,6 @@
 /**
  * title: 登录
  * desc: |
- *    该示例中未用到 `ItemUserName` `ItemPassword` 的校验，所以使用 `ItemInput` 。但是也可以基于 `ItemUserName` `ItemPassword` 重置 `rules` 。
- * 
  *    ItemCaptcha 组件执行顺序： `check` 方法验证手机号码或邮箱成功后，调用 `onGetCaptcha` 并设置按钮 `loading` 状态。当 `onGetCaptcha` 执行成功后，开始倒计时。
  */
 import * as React from 'react';
@@ -49,66 +47,55 @@ const LoginDemo: React.FC<{}> = () => {
             block: true,
           }
         }}
+        labelCol={{ style: { display: 'none' } }}
+        size="large"
       >
         <ItemInput
           name="userName"
           inputProps={{
             prefix: <UserOutlined />,
-            size: 'large',
             placeholder: "请输入用户名"
           }}
           label='用户名'
-          labelCol={{ style: { display: 'none' } }}
           required
         />
         <ItemInput.Password
           name="password"
           inputProps={{
             prefix: <LockOutlined />,
-            size: 'large',
             placeholder: "请输入密码"
           }}
           label='密码'
-          labelCol={{ style: { display: 'none' } }}
           required
         />
         <ItemEmail
           name='email'
           inputProps={{
             prefix: <MailOutlined />,
-            size: 'large',
             placeholder: "请输入邮箱"
           }}
           validateTrigger="onChange"
           label='邮箱'
-          labelCol={{ style: { display: 'none' } }}
           required
         />
         <ItemMobile
           name='mobile'
           inputProps={{
             prefix: <MobileOutlined />,
-            size: 'large',
             placeholder: "请输入手机号码"
           }}
           validateTrigger="onChange"
           label='手机号码'
-          labelCol={{ style: { display: 'none' } }}
           required
         />
         <ItemCaptcha
           name="captcha"
           inputProps={{
             prefix: <SafetyCertificateOutlined />,
-            size: 'large',
             placeholder: '请输入验证码'
-          }}
-          buttonProps={{
-            size: 'large'
           }}
           required
           label="验证码"
-          labelCol={{ style: { display: 'none' } }}
           check={() => {
             // 验证手机号码或邮箱是否正确
             return form.validateFields(['mobile']).catch(() => {
@@ -125,12 +112,10 @@ const LoginDemo: React.FC<{}> = () => {
           name='verifyCode'
           inputProps={{
             prefix: <SafetyCertificateOutlined />,
-            size: 'large',
             placeholder: "请输入图片验证码"
           }}
           after={<img src="http://img3.itboth.com/12/14/qAVNBz.jpg" style={{ height: 40, margin: 0 }} />}
           label='图片验证码'
-          labelCol={{ style: { display: 'none' } }}
           required
         />
       </BizForm>
