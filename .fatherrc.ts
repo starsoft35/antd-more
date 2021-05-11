f64736f603e402249ebcd8c0959af5e4cf86e3a5
蔡金锋 <jeff.cai@99bill.com>
2021-05-12 01:10:14 +0800

    docs: 更新文档

--- .fatherrc.ts
+++ .fatherrc.ts
@@ -7,17 +7,6 @@ export default {
     type: 'babel',
     lazy: true
   },
-  // 暂不支持 umd，需要重新配置
-  // umd: {
-  //   file: 'antd-more',
-  //   globals: {
-  //     react: 'React',
-  //     antd: 'antd',
-  //     // '@ant-design/icons': '@ant-design/icons' // ?
-  //   },
-  //   sourcemap: true,
-  //   minFile: true
-  // },
   runtimeHelpers: true,
   extraBabelPlugins: [
     [
