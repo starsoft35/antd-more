6577ba148cbc2b65d442b4db6abe4a815ef5a1f2
蔡金锋 <jeff.cai@99bill.com>
2021-10-08 15:38:18 +0800

    chore: 更新依赖版本

--- src/biz-table/usePagination.tsx
+++ src/biz-table/usePagination.tsx
@@ -1,6 +1,6 @@
 import { useState, useEffect, useCallback, useRef } from 'react';
 import { useAsync } from 'rc-hooks';
-import type { AsyncOptions, AsyncFunction } from 'rc-hooks/es/useAsync';
+import type { AsyncBaseOptions, AsyncFunction } from 'rc-hooks';
 import actionCache from './_util/actionCache';
 import type { RequestFilters, RequestSorter, RequestExtra, AsyncFnReturn } from './interface';
 
@@ -11,7 +11,7 @@ interface ParamsRef<DataType> {
   extra: RequestExtra<DataType>;
 }
 
-interface Options<DataType = any> extends AsyncOptions<DataType> {
+interface Options<DataType = any> extends AsyncBaseOptions<DataType> {
   defaultTotal?: number;
   defaultPageSize?: number;
   actionCacheKey?: string;
