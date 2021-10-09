d76a1e6d88913d4dd59b0b31bfe0f5c872458d88
蔡金锋 <jeff.cai@99bill.com>
2021-10-09 11:33:03 +0800

    docs: biztable增加searchform引用示例

---
+++ src/biz-table/demos/search-form-ref-1.tsx
@@ -0,0 +1,121 @@
+import * as React from 'react';
+import { BizTable } from 'antd-more';
+import type { BizTableRequest, BizTableColumnType } from 'antd-more';
+import { divide } from 'util-helpers';
+import { getApplyList } from './service';
+import { FormInstance } from 'antd';
+import { BizTableActionType } from '..';
+
+type DataItem = {
+  applyCode: string;
+  applicantName: string;
+  approverName: string;
+  createTime: string;
+  approveTime: string;
+  money: number;
+}
+
+const columns: BizTableColumnType<DataItem> = [
+  {
+    valueType: "indexBorder"
+  },
+  {
+    dataIndex: "applyCode",
+    title: "申请编号",
+    tooltip: "提示文字",
+    search: true
+  },
+  {
+    dataIndex: "money",
+    title: "金额",
+    valueType: "money",
+    align: "right",
+    // 写法一，传递给 BizField的参数
+    field: {
+      formatValue: value => divide(value, 100), // 分转元
+      prefix: "¥"
+    },
+    // 写法二，可关联当前数据配置
+    // field: (text, record, index) => {
+    //   console.log(text, record, index);
+    //   return {
+    //     formatValue: value => divide(value, 100), // 分转元
+    //     prefix: `${record.applicantName} `,
+    //   }
+    // },
+    order: 2
+  },
+  {
+    dataIndex: "createTime",
+    title: "提交时间",
+    valueType: "dateTime",
+    search: {
+      valueType: "date"
+    },
+    order: 2
+  },
+  {
+    dataIndex: "applicantName",
+    title: "经办员"
+  },
+  {
+    dataIndex: "approveTime",
+    title: "审核时间",
+    tooltip: "提示文字",
+    sorter: true,
+    valueType: "dateTime",
+    search: {
+      valueType: "dateTimeRange",
+      names: ["startTime", "endTime"],
+      colProps: { lg: 12, md: 24 }
+    },
+    order: 10
+  },
+  {
+    dataIndex: "approverName",
+    title: "审核员",
+    search: true
+  }
+];
+
+const Demo: React.FC = () => {
+  const formRef = React.useRef<FormInstance>();
+  const actionRef = React.useRef<BizTableActionType>();
+  const handleRequest: BizTableRequest<DataItem> = React.useCallback((params, filters, sorter, extra) => {
+    const { pageSize, current, ...restParams } = params;
+    console.log(params, filters, sorter, extra);
+    return getApplyList({
+      page: {
+        pageSize,
+        pageNum: current
+      },
+      data: restParams
+    }).then((res: any) => {
+      return {
+        total: res.pageInfo.total,
+        ...res
+      }
+    });
+  }, []);
+
+  React.useEffect(() => {
+    formRef.current.setFieldsValue({
+      approverName: '123'
+    });
+    actionRef.current.submit();
+  }, []);
+
+  return (
+    <BizTable<DataItem>
+      columns={columns}
+      rowKey="applyCode"
+      request={handleRequest}
+      toolbarAction
+      autoRequest={false}
+      formRef={formRef}
+      actionRef={actionRef}
+    />
+  );
+}
+
+export default Demo;
